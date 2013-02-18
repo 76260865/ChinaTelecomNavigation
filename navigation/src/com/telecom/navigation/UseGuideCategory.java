647dda4fb271b9e4e5b0ac0e4f9a4e79d345c987
Jason.He <76260865@qq.com>
2013-02-18 22:19:03 +0800

    added use guide category and details layout

--- navigation/src/com/telecom/navigation/UseGuideCategory.java
+++ navigation/src/com/telecom/navigation/UseGuideCategory.java
@@ -1,18 +1,11 @@
 package com.telecom.navigation;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 public class UseGuideCategory extends Activity {
 
@@ -24,58 +17,24 @@ public class UseGuideCategory extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.use_guide_layout);
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
-        gridview.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(UseGuideCategory.this, "" + position, Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
-    private class ImageAdapter extends BaseAdapter {
-        private Context mContext;
-
-        public ImageAdapter(Context c) {
-            mContext = c;
+    public void onClick(View view) {
+        switch (view.getId()) {
+        case R.id.img_type_phone:
+            break;
+        case R.id.img_type_mail:
+            break;
+        case R.id.img_type_internet:
+            break;
+        case R.id.img_type_camera:
+            break;
+        case R.id.img_type_media:
+            break;
+        case R.id.img_type_album:
+            break;
         }
-
-        @Override
-        public int getCount() {
-            // TODO Auto-generated method stub
-            return mThumbIds.length;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            // TODO Auto-generated method stub
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            // TODO Auto-generated method stub
-
-            ImageView imageview;
-            if (convertView == null) {
-                imageview = new ImageView(mContext);
-                imageview.setLayoutParams(new GridView.LayoutParams(341, 260));
-                imageview.setBackgroundResource(R.drawable.app_detail_background);
-            } else {
-                imageview = (ImageView) convertView;
-            }
-            imageview.setImageResource(mThumbIds[position]);
-            return imageview;
-        }
-
-        private Integer[] mThumbIds = { R.drawable.techingtype2, R.drawable.techingtype3,
-                R.drawable.techingtype4, R.drawable.techingtype5, R.drawable.techingtype6,
-                R.drawable.techingtype1 };
+        Intent intent = new Intent(this, UseGuideDetail.class);
+        startActivity(intent);
     }
 }
---
+++ navigation/src/com/telecom/navigation/UseGuideDetail.java
@@ -0,0 +1,117 @@
+package com.telecom.navigation;
+
+import com.telecom.navigation.AdvertisementActivity.AdvertisementFragment;
+import com.telecom.navigation.AdvertisementActivity.AdvertisementFragmentAdapter;
+import com.telecom.view.CirclePageIndicator;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class UseGuideDetail extends FragmentActivity {
+    AdvertisementFragmentAdapter mAdapter;
+    ViewPager mPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // The look of this sample is set via a style in the manifest
+        setContentView(R.layout.use_guide_detail);
+
+        mAdapter = new AdvertisementFragmentAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        CirclePageIndicator indicator = (CirclePageIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+        indicator.setBackgroundColor(Color.BLACK);
+    }
+
+    class AdvertisementFragmentAdapter extends FragmentPagerAdapter {
+        protected final int[] CONTENT = new int[] { R.drawable.ad1, R.drawable.ad2, R.drawable.ad3 };
+
+        private int mCount = CONTENT.length;
+
+        public AdvertisementFragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return AdvertisementFragment.newInstance(CONTENT[position % CONTENT.length]);
+        }
+
+        @Override
+        public int getCount() {
+            return mCount;
+        }
+
+        public void setCount(int count) {
+            if (count > 0 && count <= 10) {
+                mCount = count;
+                notifyDataSetChanged();
+            }
+        }
+    }
+
+    public final static class AdvertisementFragment extends Fragment {
+        private static final String KEY_CONTENT = "AdvertisementFragment:Content";
+
+        private int mContent;
+
+        public static AdvertisementFragment newInstance(int content) {
+            AdvertisementFragment fragment = new AdvertisementFragment();
+
+            fragment.mContent = content;
+
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(final LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            if ((savedInstanceState != null) && savedInstanceState.containsKey(KEY_CONTENT)) {
+                mContent = savedInstanceState.getInt(KEY_CONTENT);
+            }
+
+            ImageView view = (ImageView) inflater.inflate(R.layout.advertisement_item_layout, null);
+            view.setImageResource(mContent);
+
+            if (mContent == R.drawable.ad3) {
+                view.setOnClickListener(new OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getActivity(), AuthenticationActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+            return view;
+        }
+
+        @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            outState.putInt(KEY_CONTENT, mContent);
+        }
+    }
+}
